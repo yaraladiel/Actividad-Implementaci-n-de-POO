c7aab124b4c80fa5d0c12036d29737774eceb91f
yaraladiel <yaraladiel@gmail.com>
2026-02-21 11:40:31 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Spectre.Console;
+
+namespace Amortization
+{
+    public class solicitudDatos
+    {
+        public decimal montoPrestamo { get; set; }
+        public decimal interesAnual { get; set; }
+        public int plazoMeses { get; set; }
+           
+       
+
+    }
+}
